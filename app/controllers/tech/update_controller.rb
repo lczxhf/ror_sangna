ae41007453f9e13a324b42970888c8645bc82762
shindouhiro <274173368@qq.com>
2015-07-27 17:09:31 +0800

    update.json update url

--- app/controllers/tech/update_controller.rb
+++ app/controllers/tech/update_controller.rb
@@ -13,7 +13,7 @@ class Tech::UpdateController < ApplicationController
     	"version":"0.2",
     	"title":"Hello H5+(0.7.0)版本更新",
     	"note":"新增自动升级检测功能\n新增分享功能演示页面\n新增推送功能演示页面\n",
-    	"url":"http://192.168.1.99:3000/app/H5D747740_0727114736.apk"
+    	"url":"http://weixin.linkke.cn/app/linke.apk"
     }
 }'
 	end
