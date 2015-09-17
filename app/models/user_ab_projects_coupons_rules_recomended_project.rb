0de9ca0618b98a2eee405b403905c43aeb6b1885
lczxhf <lczxhf@sina.com>
2015-09-17 19:04:43 +0800

    delete recommended_project.rb

--- app/models/user_ab_projects_coupons_rules_recomended_project.rb
+++
@@ -1,6 +0,0 @@
-class UserAbProjectsCouponsRulesRecommendedProject < ActiveRecord::Base
-	has_many :coupons_records,foreign_key: 'projects_id'
-	belongs_to :ab_rule,class_name: 'UserAbProjectsCouponsRule'
-	belongs_to :per_user,foreign_key:'user_id'
-	belongs_to :per_user_project,foreign_key: 'projects_id'
-end
